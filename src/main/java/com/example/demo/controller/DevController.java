d60127bf61894bbf88e73384779bdb2a57a190b6
aiden <zhw0809013@163.com>
2020-10-25 19:55:03 +0800

    test diff

--- src/main/java/com/example/demo/controller/DevController.java
+++ src/main/java/com/example/demo/controller/DevController.java
@@ -4,4 +4,9 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class DevController {
+	
+	public String getMasterInfo324() {
+
+        return "换了家公司，希望可以承受着压力!";
+    }
 }
